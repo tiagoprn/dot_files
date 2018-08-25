062ae6714d22470a40228b154436da60387e7b0d
tiago.paranhos <tiagoprn@gmail.com>
2018-08-25 12:47:08 -0300

    Adding conky configuration.

---
+++ .Conky/startconky.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sleep 10 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
+conky -c ~/.Conky/rings & # the main conky with rings
+sleep 8 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
+conky -c ~/.Conky/cpu &
+sleep 1
+conky -c ~/.Conky/mem &
+if [[ ! -e /storage/temp/notes.txt ]]; then
+    echo 'Put some notes on /storage/temp/notes.txt to show here.' > /storage/temp/notes.txt
+fi
+conky -c ~/.Conky/notes &
---
+++ .Conky/viewnotes.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat /storage/temp/notes.txt
