a7b737a0cadb9f46a08e9b9d956d00b22fc322a7
tiago.paranhos <tiagoprn@gmail.com>
2018-10-03 10:25:01 -0300

    Removing conky rings.

--- .Conky/startconky.sh
+++ .Conky/startconky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sleep 10 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
-conky -c ~/.Conky/rings & # the main conky with rings
+# sleep 10 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
+# conky -c ~/.Conky/rings & # the main conky with rings
 sleep 8 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
 conky -c ~/.Conky/cpu &
 sleep 1
