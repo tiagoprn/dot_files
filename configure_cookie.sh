8e003476c11ee736eebc3c300d2232ff31e06e93
tiago.paranhos <tiagoprn@gmail.com>
2019-09-08 11:49:40 -0300

    Updated configuration scripts.

--- configure_cookie.sh
+++ configure_cookie.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -fr ~/.cookiecutters
 ln -s /storage/src/devops/.cookiecutters ~/.cookiecutters && \
 cd /tmp && \
 git clone https://github.com/bbugyi200/cookie && \
--- configure_git.sh
+++ configure_git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -fr ~/.gitconfig ~/.gitign*
 ln -s /storage/src/dot_files/.gitconfig ~/.gitconfig
 ln -s /storage/src/dot_files/.gitignore.global ~/.gitignore.global
 git config --global core.excludesfile ~/.gitignore.global
