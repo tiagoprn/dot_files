3717d6e58001db3d5cd16c1a622ce9851f3a91c0
tiago.paranhos <tiagoprn@gmail.com>
2020-01-05 11:03:42 -0300

    Updated

---
+++ configure_bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TIMESTAMP="$(date "+%Y%m%d.%H%M.%S")"
+mv ~/.bash_aliases ~/.bash_aliases.$TIMESTAMP.snapshot
+mv ~/.bash_functions ~/.bashrc_functions.$TIMESTAMP.snapshot
+mv ~/.bash_environment ~/.bash_environment.$TIMESTAMP.snapshot
+mv ~/.bashrc ~/.bashrc.$TIMESTAMP.snapshot
+ln -s /storage/src/dot_files/.bash_aliases ~/.bash_aliases
+ln -s /storage/src/dot_files/.bash_functions ~/.bash_functions
+ln -s /storage/src/dot_files/.bash_environment ~/.bash_environment
+ln -s /storage/src/dot_files/.bashrc ~/.bashrc
