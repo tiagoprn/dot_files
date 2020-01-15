0c397a240d3816de1f058065a4bd4b7c40b6f19c
tiago.paranhos <tiagoprn@gmail.com>
2020-01-15 08:26:28 -0300

    Adding a .inputrc

--- configure_bash.sh
+++ configure_bash.sh
@@ -4,7 +4,9 @@ mv ~/.bash_aliases ~/.bash_aliases.$TIMESTAMP.snapshot
 mv ~/.bash_functions ~/.bashrc_functions.$TIMESTAMP.snapshot
 mv ~/.bash_environment ~/.bash_environment.$TIMESTAMP.snapshot
 mv ~/.bashrc ~/.bashrc.$TIMESTAMP.snapshot
+mv ~/.inputrc ~/.inputrc.$TIMESTAMP.snapshot
 ln -s /storage/src/dot_files/.bash_aliases ~/.bash_aliases
 ln -s /storage/src/dot_files/.bash_functions ~/.bash_functions
 ln -s /storage/src/dot_files/.bash_environment ~/.bash_environment
 ln -s /storage/src/dot_files/.bashrc ~/.bashrc
+ln -s /storage/src/dot_files/.inputrc ~/.inputrc
