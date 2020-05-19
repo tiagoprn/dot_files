9bfe14158f0dff0c45e3a55a68e65cbbfa01b3bd
tiago.paranhos <tiagoprn@gmail.com>
2020-05-19 07:49:06 -0300

    Updated script that reconfigures vim.

--- configure_vim.sh
+++ configure_vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -fr ~/.vim ~/.vimrc ; ln -s /storage/src/dot_files/.vimrc ~/.vimrc && \
+rm -fr ~/.vim ~/.vimrc ~/.vimi* .vim-*; ln -s /storage/src/dot_files/.vimrc ~/.vimrc && \
 ln -s /storage/src/dot_files/.vim ~/.vim && rm -fr ~/.vim/bundle && mkdir ~/.vim/bundle && \
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && \
 vim +PluginInstall +all
