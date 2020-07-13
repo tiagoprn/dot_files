e5e21cbfdda6092f0e5cdb525793b403cc1ac317
tiago.paranhos <tiagoprn@gmail.com>
2020-07-13 06:49:34 -0300

    Updated addin ropevim

--- configure_vim.sh
+++ configure_vim.sh
@@ -2,4 +2,5 @@
 rm -fr ~/.vim ~/.vimrc ~/.vimi* .vim-* ~/vim-sessions; ln -s /storage/src/dot_files/.vimrc ~/.vimrc && \
 ln -s /storage/src/dot_files/.vim ~/.vim && rm -fr ~/.vim/bundle && mkdir ~/.vim/bundle && \
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
+wget -P ~/.vim/ https://raw.githubusercontent.com/python-rope/ropevim/master/ftplugin/python_ropevim.vim && \
 vim +PluginInstall +all
