051056140f370fc318df206e80203622b11bf896
tiago.paranhos <tiagoprn@gmail.com>
2021-01-04 09:14:14 -0300

    feat(pii): update conf

--- raspberry/pii/sync-dot-files-to-git-repo.sh
+++ raspberry/pii/sync-dot-files-to-git-repo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 REPO_PATH=/storage/src/dot_files/raspberry/pii
-ORIGIN=".bashrc .bash_aliases .bash_functions .dmrc .profile .tmux.conf welcome.txt picom.conf i3 .Xresources .xinitrc dunstrc"
+ORIGIN=".bashrc .bash_aliases .bash_functions .dmrc .profile .tmux.conf welcome.txt picom.conf i3 .Xresources .xinitrc"
 for unit in $ORIGIN; do
 	# ls -lha "$unit";
 	cp -farv "$unit" "$REPO_PATH";
 done
+
+
+cp -farv "$HOME"/.config/dunst "$REPO_PATH"
--- raspberry/pii/sync-to-home.sh
+++ raspberry/pii/sync-to-home.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-ORIGIN=".bashrc .bash_aliases .bash_functions .dmrc .profile .tmux.conf welcome.txt picom.conf i3 .Xresources dunstrc"
+ORIGIN=".bashrc .bash_aliases .bash_functions .dmrc .profile .tmux.conf welcome.txt picom.conf i3 .Xresources"
 for unit in $ORIGIN; do
 	# ls -lha "$unit";
 	cp -farv "$unit" "$HOME";
 done
+
+cp -farv dunst "$HOME"/.config/dunst
