0d6ca2fa3be31254105bb07d80859adbc40f9ee3
tiago.paranhos <tiagoprn@gmail.com>
2021-01-12 11:17:36 -0300

    feat: Updated the xsession to run dwm.

--- configure_bash.sh
+++ configure_bash.sh
@@ -6,6 +6,7 @@ mv ~/.bash_environment ~/.bash_environment.$TIMESTAMP.snapshot || true
 mv ~/.bashrc ~/.bashrc.$TIMESTAMP.snapshot || true
 mv ~/.inputrc ~/.inputrc.$TIMESTAMP.snapshot || true
 mv ~/.fonts ~/.fonts.$TIMESTAMP.snapshot || true
+mv ~/.xsession ~/.xsession.$TIMESTAMP.snapshot || true
 
 ln -s /storage/src/dot_files/.bash_aliases ~/.bash_aliases
 ln -s /storage/src/dot_files/.bash_functions ~/.bash_functions
@@ -13,6 +14,7 @@ ln -s /storage/src/dot_files/.bash_environment ~/.bash_environment
 ln -s /storage/src/dot_files/.bashrc ~/.bashrc
 ln -s /storage/src/dot_files/.inputrc ~/.inputrc
 ln -s /storage/src/dot_files/.fonts ~/.fonts
+ln -s /storage/src/dot_files/.xsession ~/.xsession
 ln -s /storage/src/dot_files/.navirc ~/.navirc
 
 fc-cache -vf ~/.fonts/ && echo 'listing fonts...' && fc-list
