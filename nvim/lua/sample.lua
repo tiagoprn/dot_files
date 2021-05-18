2f0d072322f2cf3e245de6ff64cf0523cccecf0a
tiago.paranhos <tiagoprn@gmail.com>
2021-05-18 07:17:08 -0300

    feat(nvim): reverting

--- nvim/lua/sample.lua
+++ nvim/lua/sample.lua
@@ -6,9 +6,6 @@ local M = {}  -- creates a new table here to isolate from the global scope
 function M.welcomeToLua()
   -- api.nvim_command('enew') -- equivalent to :enew
   api.nvim_command('echo "Welcome to lua! o/"')
-  print ("Who are you?") ;
-  answer = io.read ();
-  print ("YOU TYPED: "+answer)
 end
 
 return M
