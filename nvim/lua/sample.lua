ba4dc53a37019c4699a2fd8d933b59da83bc9e45
tiago.paranhos <tiagoprn@gmail.com>
2021-05-20 08:35:45 -0300

    feat(nvim): added new vim samples.

--- nvim/lua/sample.lua
+++ nvim/lua/sample.lua
@@ -1,11 +1,29 @@
--- sample lua function
+-- sample lua functions
 
-local api = vim.api
+local command = vim.api.nvim_command
+local fn = vim.fn
+
 local M = {}  -- creates a new table here to isolate from the global scope
 
+-- This function shows how to run vim commands
 function M.welcomeToLua()
-  -- api.nvim_command('enew') -- equivalent to :enew
-  api.nvim_command('echo "Welcome to lua! o/"')
+  -- command 'enew'  -- equivalent to :enew
+  command 'echo "Welcome to lua! o/"'
 end
+
+function M.runExternalCommand()
+  local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+  command('!ls -lha '..install_path)
+end
+
+function M.checkForErrorsAsBooleanVariable()
+  local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+  if packer_exists then
+    command 'echo "Packer exists! o/"'
+  else
+    command 'echo "Packer DOES NOT exist! :("'
+  end
+end
+
 
 return M
