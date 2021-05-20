30dc0e71f25151fce67486d0ef44792b35c21abe
tiago.paranhos <tiagoprn@gmail.com>
2021-05-20 06:51:38 -0300

    fix(nvim): setting up packer.

--- configure_neovim.sh
+++ configure_neovim.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+echo 'Deleting old configuration (if it exists)...'
+
 rm -fr ~/.config/nvim || true && mkdir -p ~/.config
 ln -s /storage/src/dot_files/nvim ~/.config/nvim
-git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim +PackerSync +qa
+# git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# nvim +PackerSync +qa
+
+echo 'DONE. Configuration was syslinked to my dot_files repo.'
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,9 +1,39 @@
 -- To more advanced package configuration: https://github.com/wbthomason/packer.nvim#quickstart
 -- (on this link there also information on how to install lua rocks (packages))
+-- Here is another reference I used to setup this file: https://gist.github.com/salkin-mada/99faddbeaba20fa551a8813a549b4027
+
+
+-- check if packer is installed (~/local/share/nvim/site/pack)
+local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+
+if not packer_exists then
+	if vim.fn.input("Hent packer.nvim? (y for yada)") ~= "y" then
+		return
+	end
+
+	local directory = string.format(
+	'%s/site/pack/packer/opt/',
+	vim.fn.stdpath('data')
+	)
+
+	vim.fn.mkdir(directory, 'p')
+
+	local git_clone_cmd = vim.fn.system(string.format(
+	'git clone %s %s',
+	'https://github.com/wbthomason/packer.nvim',
+	directory .. '/packer.nvim'
+	))
+
+	print(git_clone_cmd)
+	print("Henter packer.nvim...")
+
+	return
+end
+
 
 return require('packer').startup(function()
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use {'wbthomason/packer.nvim'}
   use {
     'nvim-telescope/telescope.nvim',
     requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
