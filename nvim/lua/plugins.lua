07e9a8bb262128c99de2c2e1cd101937e69a1258
tiago.paranhos <tiagoprn@gmail.com>
2021-05-24 05:44:19 -0300

    feat(nvim): adding vim-surround.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,4 +8,5 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim',
     requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
   }
+  use {'tpope/vim-surround'}
 end)
