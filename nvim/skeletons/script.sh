dc39dfbc30f9a88a8ffe66941cbc1cf6e06c32fc
tiago.paranhos <tiagoprn@gmail.com>
2021-07-08 06:53:30 -0300

    feat(nvim): updated shellscript skeleton.

--- nvim/skeletons/script.sh
+++ nvim/skeletons/script.sh
@@ -2,3 +2,20 @@
 
 set -eou pipefail
 
+usage()
+{
+	echo "usage: script.sh -p value"
+}
+
+no_args="true"
+while getopts ":p:" arg; do
+  case $arg in
+    p) VALUE=$OPTARG;;
+  esac
+  no_args="false"
+done
+
+[[ "$no_args" == "true" ]] && { usage; exit 1; }
+
+echo -e "VALUE=$VALUE"
+
