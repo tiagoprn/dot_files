fc117794347afc06a4881d5a96bedec822f247f9
tiago.paranhos <tiagoprn@gmail.com>
2021-08-28 10:10:42 -0300

    feat: configuring python-lsp.

--- nvim/lua/python-lsp.lua
+++ nvim/lua/python-lsp.lua
@@ -1,3 +1,15 @@
 -- assumes python-language-server[all] installed from pip
-require'lspconfig'.pyls.setup{}
+require'lspconfig'.pyls.setup{
+        settings = {
+            pyls={
+                plugins = {
+                    pylint = {
+                        enabled = true,
+                        executable = 'pylint',
+                        args={'--rcfile', '/storage/temp/minapi/.pylintrc'}
+                    }
+                }
+            }
+        }
+}
 
