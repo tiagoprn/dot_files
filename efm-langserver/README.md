0d2338e7b8f6ae75beb03b993795f5b5ca0a647e
tiago.paranhos <tiagoprn@gmail.com>
2021-09-09 06:47:43 -0300

    feat(nvim): finetuning efm language server.

---
+++ efm-langserver/README.md
@@ -0,0 +1,3 @@
+This configures the language servers for all languages.
+
+It is expected at `$HOME/.config/efm-langserver/config.yaml`, as stated at <https://github.com/mattn/efm-langserver#example-for-configyaml>.
--- nvim/lua/efm-lsp.lua
+++ nvim/lua/efm-lsp.lua
@@ -1,4 +1,5 @@
--- for the efm linters and formatters configuration, check `efm-langserver/config.yaml` on this repository.
+-- IMPORTANT: for the efm linters and formatters configuration, check `efm-langserver/config.yaml` on this repository.
+
 require'lspconfig'.efm.setup{
     init_options = {documentFormatting = true},
 }
