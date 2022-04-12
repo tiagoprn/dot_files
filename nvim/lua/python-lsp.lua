e548eaa0ee7153e9b5a381b58927b261fdd245aa
tiago.paranhos <tiagoprn@gmail.com>
2022-04-12 17:22:42 -0300

    feat(nvim): updated lsp configuration to disable formatting capabilities.

--- nvim/lua/python-lsp.lua
+++ nvim/lua/python-lsp.lua
@@ -3,16 +3,11 @@ local lsp=require('lspconfig')
 
 lsp.pylsp.setup{
   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  settings = {
-      pylsp = {
-          plugins = {
-              pylint = {
-                  enabled = true,
-                  executable = 'pylint',
-                  args={'--rcfile', '/storage/src/devops/python/default_configs/.pylintrc'}
-              }
-          }
-      }
-  }
+  -- disabled formatting capabilities because they are provided py
+  -- efm-langserver, which has configuration for all languages.
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end,
 }
 
