630197394e6ea52aaf3459530d6ecaabb48ee1e3
tiago.paranhos <tiagoprn@gmail.com>
2022-07-10 19:42:58 -0300

    feat: more enhancements for cosmos.

--- bash-welcome.sh
+++ bash-welcome.sh
@@ -2,6 +2,17 @@
 
 set -eou pipefail
 
+# is not inside tmux:
+if [ -z "${TMUX+set}" ]; then
+    if [[ $HOSTNAME == cosmos ]]; then
+        echo -e 'To lock/unlock the GOCRYPTFS ENCRYPTED VAULT:'
+        echo -e 'GET PASSWORD...: ~/vault-get-password.sh'
+        echo -e 'MOUNT..........: ~/vault-mount.sh'
+        echo -e 'UMOUNT.........: ~/vault-umount.sh'
+        echo -e '---'
+    fi
+fi
+
 cowsay -b -f tux -W 80 $(fortune -u -e -c /storage/src/fortunes/ | tail +3)
 
 # if [ -x "$(command -v figlet)" ]; then
