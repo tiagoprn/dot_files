71551ff5e2cb26a36b60c1e41cf3d754d4b94da6
tiago.paranhos <tiagoprn@gmail.com>
2022-07-11 10:51:55 -0300

    feat(tiling-window-managers): updated keyboard-fix-ccedilha.

--- tiling-window-managers/scripts/keyboard-fix-ccedilha.sh
+++ tiling-window-managers/scripts/keyboard-fix-ccedilha.sh
@@ -2,4 +2,4 @@
 
 set -eou pipefail
 
-sudo -- bash -c "cp -farv /etc/environment /etc/environment.snapshot.$(date +%Y%m%d-%H%M%S-%N) && echo 'GTK_IM_MODULE=cedilla' >> /etc/environment"
+sudo -- bash -c "cp -farv /etc/environment /etc/environment.snapshot.$(date +%Y%m%d-%H%M%S-%N) && echo -e 'GTK_IM_MODULE=cedilla\nQT_IM_MODULE=cedilla' >> /etc/environment"
