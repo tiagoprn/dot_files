0de1afe1f69429dd28830853120cd170442fd242
tiago.paranhos <tiagoprn@gmail.com>
2022-07-11 07:24:28 -0300

    feat(.screenlayout): updated machine monitor layout.

--- .screenlayout/dft-sp-wkn789/work-notebook-just-builtin-screen.sh
+++ .screenlayout/dft-sp-wkn789/work-notebook-just-builtin-screen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xrandr --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-2 --off
+xrandr --output eDP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-2 --off
 
 # setup bspwm
-bspc monitor DP-1 -d 1 2 3 4 5 6 7 8 9 10
+bspc monitor eDP-1 -d 1 2 3 4 5 6 7 8 9 10
