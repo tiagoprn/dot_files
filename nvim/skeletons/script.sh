2a44e160e8ca75cf65213d6e6d557781096d5b7c
tiago.paranhos <tiagoprn@gmail.com>
2022-07-12 07:22:07 -0300

    feat(nvim): updated skeletons.

--- nvim/skeletons/script.sh
+++ nvim/skeletons/script.sh
@@ -2,24 +2,31 @@
 
 set -eou pipefail
 
+shopt -s expand_aliases
+
 LOG_DIR="$HOME/tmp"
 SCRIPT_NAME=$(basename "$0")
 
-usage()
-{
-	echo "usage: script.sh -p value"
+usage() {
+    echo "usage: script.sh -p value"
 }
 
 no_args="true"
 while getopts ":p:" arg; do
-  case $arg in
-    p) VALUE=$OPTARG;;
-    *) echo "invalid parameter" && { usage; exit 1; }
-  esac
-  no_args="false"
+    case $arg in
+        p) VALUE=$OPTARG ;;
+        *) echo "invalid parameter" && {
+            usage
+            exit 1
+        } ;;
+    esac
+    no_args="false"
 done
 
-[[ "$no_args" == "true" ]] && { usage; exit 1; }
+[[ $no_args == "true" ]] && {
+    usage
+    exit 1
+}
 
 echo -e "VALUE=$VALUE"
 
