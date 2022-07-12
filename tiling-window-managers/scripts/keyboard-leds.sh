c11e3977c74bf4bce5a49d869b37faf53ae595ee
tiago.paranhos <tiagoprn@gmail.com>
2022-07-12 09:05:03 -0300

    feat(polybar): tuned default config.

--- tiling-window-managers/scripts/keyboard-leds.sh
+++ tiling-window-managers/scripts/keyboard-leds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-STATUS=$(xset -q | grep Caps | sed 's/0[0-2]://g' | sed 's/off/off,/g' | sed 's/on/on,/g' |  sed 's/.$//' | tr -s ' ' | tr '[a-z]' '[A-Z]' | sed 's/,/ | /g' | sed 's/LOCK//g' | sed 's/ ://g' | sed 's/  / /g' | sed 's/ | /|/g' | sed 's/CAPS/CAP/g' | sed 's/SCROLL/SCR/g' | cut -d '|' -f 1)
+STATUS=$(xset -q | grep Caps | sed 's/0[0-2]://g' | sed 's/off/off,/g' | sed 's/on/on,/g' | sed 's/.$//' | tr -s ' ' | tr '[a-z]' '[A-Z]' | sed 's/,/ | /g' | sed 's/LOCK//g' | sed 's/ ://g' | sed 's/  / /g' | sed 's/ | /|/g' | sed 's/CAPS/CAP/g' | sed 's/SCROLL/SCR/g' | cut -d '|' -f 1)
 
-echo -e "$STATUS";
+echo -e "$STATUS "
