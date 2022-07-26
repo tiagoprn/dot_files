e911cee9cda67396a48d78d7b16502b369087c7a
tiago.paranhos <tiagoprn@gmail.com>
2022-07-26 06:53:58 -0300

    feat(tmux): new tmux configuration and updated cheatsheet.

--- tiling-window-managers/scripts/tmux-dmenu-cheatsheet.sh
+++ tiling-window-managers/scripts/tmux-dmenu-cheatsheet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BINDINGS=$(cat /storage/src/dot_files/.tmux.conf | grep '^bind[-key]*' | grep '  #@ ' | sed 's/bind-key -T copy-mode-vi //g' | sed 's/bind-key -T root //g' | sed 's/bind-key //g' | sed 's/bind //g')
+BINDINGS=$(cat /storage/src/dot_files/.tmux.conf | grep '  #@ ' | sed 's/bind-key -T copy-mode-vi //g' | sed 's/bind-key -T root //g' | sed 's/bind-key //g' | sed 's/bind //g')
 
 : '
 Example output:
@@ -21,11 +21,11 @@ r source-file ~/.tmux.conf \; display-message "Config reloaded!"  #@ reload tmux
 ```
 '
 
-echo -e "$BINDINGS" | \
-	awk 'BEGIN { FS = "#@"} {word_end_index= match($1, " "); binding_text=substr($1, 0, word_end_index); print $2 " @ " binding_text}' | \
-	column -t -s '@' | \
-	sort | \
-	dmenu -fn Monospace:size=12 -c -bw 2 -l 20 -p 'Filter a tmux binding:'
+echo -e "$BINDINGS" \
+    | awk 'BEGIN { FS = "#@"} {word_end_index= match($1, " "); binding_text=substr($1, 0, word_end_index); print $2 " @ " binding_text}' \
+    | column -t -s '@' \
+    | sort \
+    | dmenu -fn Monospace:size=12 -c -bw 2 -l 20 -p 'Filter a tmux binding:'
 
 : '
 Explaining the awk language expression:
