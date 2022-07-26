b0207384b6a4a976dce4370e4e8e0353f956fa09
tiago.paranhos <tiagoprn@gmail.com>
2022-07-26 15:02:44 -0300

    fix(bash-welcome): checking if the fortunes directory exists to not turn the shell unusable.

--- bash-welcome.sh
+++ bash-welcome.sh
@@ -10,8 +10,6 @@ if [ -z "${TMUX+set}" ]; then
     fi
 fi
 
-cowsay -b -f tux -W 80 $(fortune -u -e -c /storage/src/fortunes/ | tail +3)
-
-# if [ -x "$(command -v figlet)" ]; then
-#     echo "$(hostname)" | figlet -cptk
-# fi
+if [ -d "/storage/src/fortunes/" ]; then
+    cowsay -b -f tux -W 80 $(fortune -u -e -c /storage/src/fortunes/ | tail +3)
+fi
