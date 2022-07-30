8e5b29840685f4f36e2538da7d6043db44fecfb2
tiago.paranhos <tiagoprn@gmail.com>
2022-07-30 10:30:37 -0300

    feat(bash): updated bash-welcome.

--- bash-welcome.sh
+++ bash-welcome.sh
@@ -2,14 +2,21 @@
 
 set -eou pipefail
 
-# is not inside tmux:
-if [ -z "${TMUX+set}" ]; then
-    if [[ $HOSTNAME == cosmos ]]; then
-        echo -e '- To run timeshift: sudo timeshift-gtk'
-        echo -e '---'
-    fi
-fi
-
 if [ -d "/storage/src/fortunes/" ]; then
     cowsay -b -f tux -W 80 $(fortune -u -e -c /storage/src/fortunes/ | tail +3)
 fi
+
+echo -e 'TMUX GOODIES:'
+
+# is not inside tmux:
+if [ -z "${TMUX+set}" ]; then
+    echo -e '- Consider opening tmux sessions with the "tp" bash alias.'
+fi
+
+echo -e '- Use "tmux-nvim-project-setup.sh" to create a default session on a git repository with nvim,lazygit and a scratchpad.'
+
+if [[ $HOSTNAME == cosmos ]]; then
+    echo -e '- To run timeshift: sudo timeshift-gtk'
+fi
+
+echo -e '---'
