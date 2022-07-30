f45b575d1934f0b1f11ce219c980d25fe81670d4
tiago.paranhos <tiagoprn@gmail.com>
2022-07-30 09:05:21 -0300

    feat(tiling-wms): updated create-fleeting-note script.

--- tiling-window-managers/scripts/create-fleeting-note.sh
+++ tiling-window-managers/scripts/create-fleeting-note.sh
@@ -8,7 +8,17 @@ FILENAME=$FLEETING_NOTES_DIR/$TIMESTAMP.md
 
 mkdir -p $FLEETING_NOTES_DIR
 
-nvim -c "norm Iqheader " \
-    -c 'norm gg$' \
-    -c "startinsert" \
-    $FILENAME
+SELECTED_OPTION=$(echo -e '1 - Open recently created/updated file\n2 - Create new file' | fzf | awk '{print $1}')
+
+if [ "$SELECTED_OPTION" == '1' ]; then
+    FILE_TO_OPEN=$(find . -type f -ctime -7 -name '*.md' | sort -r | fzf --prompt='Choose a file created/changed on the last 7 days > ' --preview 'bat --style numbers,changes --color=always {} | head -n 30')
+    nvim "$FILE_TO_OPEN"
+else
+    cd $FLEETING_NOTES_DIR \
+        && nvim -c "norm Iqheader " \
+            -c 'norm gg$' \
+            -c "startinsert" \
+            "$FILENAME"
+fi
+
+exit 0
