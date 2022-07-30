b637b87f5bce32f05f6c9ef6bf7f81f940a9517b
tiago.paranhos <tiagoprn@gmail.com>
2022-07-30 10:11:33 -0300

    feat(tiling-wms): updated default tmux session script and added a new related one.

---
+++ tiling-window-managers/scripts/load-notes-and-reminders-tmuxp-sessions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/fleeting-notes.yml &
+tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/reminders.yml &
+tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/notes.yml
