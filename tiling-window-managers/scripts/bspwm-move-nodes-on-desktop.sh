1fe971c93cc62dbb8f72d4f89375ef821494511d
tiago.paranhos <tiagoprn@gmail.com>
2022-08-16 14:56:29 -0300

    feat(tiling-wms): added new script.

---
+++ tiling-window-managers/scripts/bspwm-move-nodes-on-desktop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+self_name=$(basename "$0")
+
+usage() {
+    echo "Move windows from desktop x to desktop y."
+    echo "usage: $self_name -f [desktop-number] -t [desktop-number]"
+}
+
+while getopts ":f:t:" arg; do
+    case $arg in
+        f) FROM=$OPTARG ;;
+        t) TO=$OPTARG ;;
+        *) usage ;;
+    esac
+done
+
+if [ -z "${FROM+set}" ]; then
+    usage
+    exit 1
+fi
+
+if [ -z "${TO+set}" ]; then
+    usage
+    exit 1
+fi
+
+echo -e "FROM=$FROM"
+echo -e "TO=$TO"
+
+nodes=$(bspc query -N -d "$FROM" | tr '\n' ' ')
+
+for node_id in $nodes; do
+    echo -e "Moving $node_id from desktop $FROM to desktop $TO..."
+    bspc node "$node_id" -d "$TO"
+done
