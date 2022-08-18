19d0b315aa30d81cd891df37b377c0ee57a39fc2
tiago.paranhos <tiagoprn@gmail.com>
2022-08-18 07:10:33 -0300

    feat(scripts): updated notes-and-reminders tmuxp session.

--- tiling-window-managers/scripts/load-notes-and-reminders-tmuxp-sessions.sh
+++ tiling-window-managers/scripts/load-notes-and-reminders-tmuxp-sessions.sh
@@ -2,4 +2,4 @@
 
 tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/fleeting-notes.yml &
 tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/reminders.yml &
-tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/notes.yml
+tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/tasks.yml
