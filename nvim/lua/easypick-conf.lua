a780c1e3ffacdac4dc9070225f76c767f7c9aa85
tiago.paranhos <tiagoprn@gmail.com>
2022-08-19 17:36:11 -0300

    feat(nvim): customized easypick config so to be more easy to use.

--- nvim/lua/easypick-conf.lua
+++ nvim/lua/easypick-conf.lua
@@ -1,5 +1,6 @@
+-- This is plugin is useful so that I can create my custom telescope pickers.
+
 -- This allows nvim to not crash if this plugin is not installed.
--- It would be great to extend this to my other plugins configuration.
 local status_ok, easypick = pcall(require, "easypick")
 if not status_ok then
 	return
@@ -8,33 +9,31 @@ end
 -- only required for the example to work
 local base_branch = "develop"
 
-easypick.setup({
-	pickers = {
-		-- add your custom pickers here
-		-- below you can find some examples of what those can look like
-
-		-- list files inside current folder with default previewer
-		{
-			-- name for your custom picker, that can be invoked using :Easypick <name> (supports tab completion)
-			name = "ls",
-			-- the command to execute, output has to be a list of plain text entries
-			command = "ls",
-			-- specify your custom previwer, or use one of the easypick.previewers
-			previewer = easypick.previewers.default(),
-		},
-
-		-- diff current branch with base_branch and show files that changed with respective diffs in preview
-		{
-			name = "changed_files",
-			command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )",
-			previewer = easypick.previewers.branch_diff({ base_branch = base_branch }),
-		},
-
-		-- list files that have conflicts with diffs in preview
-		{
-			name = "conflicts",
-			command = "git diff --name-only --diff-filter=U --relative",
-			previewer = easypick.previewers.file_diff(),
-		},
+-- TODO: Add custom pickers here.
+local custom_pickers = {
+	-- list files inside current folder with default previewer
+	{
+		-- name for the custom picker, that can be invoked using :Easypick <name> (supports tab completion)
+		name = "ls",
+		-- the command to execute, output has to be a list of plain text entries
+		command = "ls",
+		-- specify your custom previwer, or use one of the easypick.previewers
+		previewer = easypick.previewers.default(),
 	},
-})
+
+	-- diff current branch with base_branch and show files that changed with respective diffs in preview
+	{
+		name = "changed_files",
+		command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )",
+		previewer = easypick.previewers.branch_diff({ base_branch = base_branch }),
+	},
+
+	-- list files that have conflicts with diffs in preview
+	{
+		name = "conflicts",
+		command = "git diff --name-only --diff-filter=U --relative",
+		previewer = easypick.previewers.file_diff(),
+	},
+}
+
+easypick.setup({ pickers = custom_pickers })
