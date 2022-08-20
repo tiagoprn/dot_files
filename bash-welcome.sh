e4b87d40726173b0f06d9f670f65d9d3ee325603
tiago.paranhos <tiagoprn@gmail.com>
2022-08-20 17:59:19 -0300

    feat(alacritty,bash): tuning configuration.

--- bash-welcome.sh
+++ bash-welcome.sh
@@ -2,9 +2,9 @@
 
 set -eou pipefail
 
-if [ -d "/storage/src/fortunes/" ]; then
-    cowsay -b -f tux -W 80 $(fortune -u -e -c /storage/src/fortunes/ | tail +3)
-fi
+# if [ -d "/storage/src/fortunes/" ]; then
+#     cowsay -b -f tux -W 80 $(fortune -u -e -c /storage/src/fortunes/ | tail +3)
+# fi
 
 echo -e 'TMUX GOODIES:'
 
@@ -13,10 +13,9 @@ if [ -z "${TMUX+set}" ]; then
     echo -e '- Consider opening tmux sessions with the "tp" bash alias.'
 fi
 
-echo -e '- Use "tmux-nvim-project-setup.sh" to create a default session on a git repository with nvim,lazygit and a scratchpad.'
+echo -e '- Use "tmux-nvim-project-setup.sh" to create a default session on a git repository'
+echo -e '  with nvim,lazygit and a scratchpad.'
 
 if [[ $HOSTNAME == cosmos ]]; then
     echo -e '- To run timeshift: sudo timeshift-gtk'
 fi
-
-echo -e '---'
