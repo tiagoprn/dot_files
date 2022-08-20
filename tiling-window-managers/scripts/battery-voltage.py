3be87fb9daa6d1764d0e29aa39cc8a22eb97ad9b
tiago.paranhos <tiagoprn@gmail.com>
2022-08-20 17:20:47 -0300

    feat(tiling-wms): adding script to get voltage on battery.

---
+++ tiling-window-managers/scripts/battery-voltage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+BATTERY = "BAT0"
+battery_directory = f"/sys/class/power_supply/{BATTERY}/"
+with open(battery_directory + "status", "r") as f:
+    state = f.read().strip()
+with open(battery_directory + "current_now", "r") as f:
+    current = int(f.read().strip())
+with open(battery_directory + "voltage_now", "r") as f:
+    voltage = int(f.read().strip())
+wattage = (voltage / 10**6) * (current / 10**6)
+wattage_formatted = f"{'-' if state == 'Discharging' else ''}{wattage:.2f}W"
+if state in ["Charging", "Discharging", "Not charging"]:
+    print(f"({state.lower()} {wattage_formatted})")
