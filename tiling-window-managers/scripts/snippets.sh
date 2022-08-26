7eeda9343d9de1ceb2041828dc1b8993851f3011
tiago.paranhos <tiagoprn@gmail.com>
2022-08-26 20:29:10 -0300

    feat(text_snippets): renamed and changed the script do use dmenu.

--- tiling-window-managers/scripts/snippets.sh
+++ tiling-window-managers/scripts/snippets.sh
@@ -2,12 +2,12 @@
 
 SNIPS=/storage/src/dot_files/text_snippets
 
-FILE=$(ls $SNIPS | rofi -dmenu -p 'Select a snippet to be pasted on your current cursor position')
+FILE=$(ls $SNIPS | dmenu -fn Monospace:size=12 -c -bw 2 -l 20 -p 'Select a snippet to be pasted on your current cursor position')
 
 if [ -f $SNIPS/$FILE ]; then
-	DATA=$([ -x "$SNIPS/$FILE" ] && bash "$SNIPS/$FILE" || head --bytes=-1 $SNIPS/$FILE)
-	printf "$DATA" | xclip -selection clipboard
-	printf "$DATA" | xclip -selection primary
-	sleep 1
-	xdotool key shift+Insert
+    DATA=$([ -x "$SNIPS/$FILE" ] && bash "$SNIPS/$FILE" || head --bytes=-1 $SNIPS/$FILE)
+    printf "$DATA" | xclip -selection clipboard
+    printf "$DATA" | xclip -selection primary
+    sleep 1
+    xdotool key shift+Insert
 fi
