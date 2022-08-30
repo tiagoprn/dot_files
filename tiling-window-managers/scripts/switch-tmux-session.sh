981645cc9cad45fa3faffb74230672a727112983
tiago.paranhos <tiagoprn@gmail.com>
2022-08-30 10:09:37 -0300

    feat(scripts,nvim): final bulletproof version of the switch tmux session script, updated nvim snippets.

--- tiling-window-managers/scripts/switch-tmux-session.sh
+++ tiling-window-managers/scripts/switch-tmux-session.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-set -eou pipefail
-
 shopt -s expand_aliases
 source "$HOME"/.bashrc
 
-tmux switch-client -t "$(tl | fzf -e --prompt='Choose a tmux session: ' | cut -d ':' -f 1)"
+SELECTION=$(tl | fzf -e --prompt='Choose a tmux session: ')
+
+EXIT_CODE=$?
+
+# notify-send "EXIT_CODE=$EXIT_CODE"
+
+if [ "$EXIT_CODE" == 0 ]; then
+    # notify-send "SELECTION=$SELECTION"
+    SESSION_NAME=$(echo "$SELECTION" | cut -d ':' -f 1)
+    # notify-send "SESSION_NAME=$SESSION_NAME"
+    tmux switch-client -t "$SESSION_NAME"
+fi
