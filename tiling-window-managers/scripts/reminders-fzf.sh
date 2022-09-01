6a21fb57d1bc278b4ef1d1953947be33b6ddea58
tiago.paranhos <tiagoprn@gmail.com>
2022-09-01 20:17:40 -0300

    feat: updated repository on 2022-09-01 20:17:39

--- tiling-window-managers/scripts/reminders-fzf.sh
+++ tiling-window-managers/scripts/reminders-fzf.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-TODAY=$(date +%Y/%m/%d)
-
 WEEK_DAYS=$(/storage/src/dot_files/tiling-window-managers/scripts/next-7-days.py)
 
-SELECTED_DATE=$(echo -e $WEEK_DAYS | tr ';' '\n' | fzf --border=rounded --margin=5% --preview-window=top,70% --preview='/storage/src/dot_files/tiling-window-managers/scripts/reminders-filter.sh -d {1}')
+SELECTED_DATE=$(echo -e "$WEEK_DAYS" | tr ';' '\n' | fzf --border=rounded --margin=5% --preview-window=top,70% --preview='/storage/src/dot_files/tiling-window-managers/scripts/reminders-filter.sh -d {1}')
 
-/storage/src/dot_files/tiling-window-managers/scripts/reminders-filter.sh -d $SELECTED_DATE
+/storage/src/dot_files/tiling-window-managers/scripts/reminders-filter.sh -d "$SELECTED_DATE"
