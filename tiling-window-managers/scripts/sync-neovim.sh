0f92d5c45f187e42a3e23e2eb4cc5755c98285c7
tiago.paranhos <tiagoprn@gmail.com>
2022-09-01 16:00:13 -0300

    feat(scripts): added option to backup neovim binary before the update.

--- tiling-window-managers/scripts/sync-neovim.sh
+++ tiling-window-managers/scripts/sync-neovim.sh
@@ -4,6 +4,15 @@
 
 PREVIOUS_VERSION=$(sudo -- bash -c 'cd /opt/src/neovim && git log -n 1 --pretty=format:"%cD by %an (%h)"')
 
+BACKUPS_ROOT=/opt/src/neovim/tmp/OLD-VERSIONS-ARCHIVE
+SUFFIX="$(date +%Y%m%d-%H%M%S-%N)"
+BACKUPS_DIR=$BACKUPS_ROOT/$SUFFIX
+NVIM_BINARY_PATH=/usr/local/bin/nvim
+
+sudo mkdir -p $BACKUPS_DIR
+sudo cp -farv $NVIM_BINARY_PATH $BACKUPS_DIR
+notify-send "$(basename "$0")" "The old nvim binary was copied to $BACKUPS_DIR in case you need to manually revert it to $NVIM_BINARY_PATH."
+
 notify-send "$(basename "$0")" "Compiling nvim (this will take a while)..."
 COMMANDS="cd /opt/src/neovim && git fetch && git pull && make clean && make CMAKE_BUILD_TYPE=Release && make install"
 sudo -- bash -c "$COMMANDS"
