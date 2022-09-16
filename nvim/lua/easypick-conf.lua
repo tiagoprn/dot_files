4f27bee7c47004ce2ff80e316f3a4b2d4e6c547d
tiago.paranhos <tiagoprn@gmail.com>
2022-09-16 16:47:03 -0300

    docs(nvim): updated easypick documentation.

--- nvim/lua/easypick-conf.lua
+++ nvim/lua/easypick-conf.lua
@@ -1,4 +1,8 @@
 -- This is plugin is useful so that I can create my custom telescope pickers.
+-- Examples: https://github.com/axkirillov/easypick.nvim/wiki
+
+-- How to use:
+-- :Easypick <name> (press <Tab> to cycle through available pickers)
 
 -- This allows nvim to not crash if this plugin is not installed.
 local status_ok, easypick = pcall(require, "easypick")
@@ -9,7 +13,17 @@ end
 -- only required for the example to work
 local base_branch = "develop"
 
+-- a list of commands that you want to pick from
+local list = [[
+<< EOF
+:PackerInstall
+:Telescope find_files
+:Git blame
+EOF
+]]
+
 -- TODO: Add custom pickers here.
+-- TODO: add a picker to select commands from Makefile and pass it through run with harpoon on tmux pane below
 local custom_pickers = {
 	-- list files inside current folder with default previewer
 	{
@@ -33,6 +47,16 @@ local custom_pickers = {
 		name = "conflicts",
 		command = "git diff --name-only --diff-filter=U --relative",
 		previewer = easypick.previewers.file_diff(),
+	},
+
+	-- create a command palette
+	{
+		name = "command_palette",
+		command = "cat " .. list,
+		-- pass a pre-configured action that runs the command
+		action = easypick.actions.run_nvim_command,
+		-- you can specify any theme you want, but the dropdown looks good for this example =)
+		opts = require("telescope.themes").get_dropdown({}),
 	},
 }
 
