54d04d1894c9aa0e17ca903ae9eb7b080cc570cb
tiago.paranhos <tiagoprn@gmail.com>
2022-11-03 07:54:10 -0300

    feat(bash-tiling_wms): updated bash to include nvm path; tiling-wms: updated scratchpad script to allow customization per machine+user.

--- .bashrc
+++ .bashrc
@@ -257,3 +257,7 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- tiling-window-managers/scripts/terminal-scratchpad_notes_tasks_n_reminders.sh
+++ tiling-window-managers/scripts/terminal-scratchpad_notes_tasks_n_reminders.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# urxvt --hold --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c 'tmuxp load /storage/src/devops/tmuxp/notes-and-reminders.yml' &
-HOSTNAME=$(hostname)
+SCRIPT_NAME=$(basename "$0")
 
-SESSIONS_SCRIPT=/storage/src/dot_files/tiling-window-managers/scripts/load-notes-and-reminders-tmuxp-sessions.sh
+HOSTNAME_USER="$(hostname).$(whoami)"
 
-if [ "$HOSTNAME" == 'dft-sp-wkn789' ]; then
-    alacritty --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c 'tmuxp load /storage/src/dafiti_shellscripts/tmuxp/notes.yml' --hold &
+if [ "$HOSTNAME_USER" == 'cosmos.tiago' ]; then
+    SESSIONS_SCRIPT=/storage/src/dot_files/tiling-window-managers/scripts/load-notes-and-reminders-tmuxp-sessions.sh
+elif [ "$HOSTNAME_USER" == 'cosmos.tds' ]; then
+    SESSIONS_SCRIPT=$HOME/contractors/octerra/git/octerra/scripts/load-octerra-tmuxp-sessions.sh
 else
-    alacritty --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c "$SESSIONS_SCRIPT" --hold &
-    # alacritty --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c "htop" --hold &
+    notify-send -u critical "$SCRIPT_NAME" "Cannot determine a sessions script, configure it on '$SCRIPT_NAME'!"
+    exit 1
 fi
+
+alacritty --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c "$SESSIONS_SCRIPT" --hold &
+# urxvt --hold --title scratchpad_notes_tasks_n_reminders -e /bin/bash -c 'tmuxp load /storage/src/devops/tmuxp/notes-and-reminders.yml' &
