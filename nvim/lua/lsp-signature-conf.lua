e0e75e6724c36911e8bce52772e0cab7e9eb5e9f
tiago.paranhos <tiagoprn@gmail.com>
2022-11-04 14:36:31 -0300

    feat(nvim): update signature plugin to show window above current line

--- nvim/lua/lsp-signature-conf.lua
+++ nvim/lua/lsp-signature-conf.lua
@@ -1,8 +1,8 @@
 require("lsp_signature").setup({
-	bind = true,
-	floating_window_above_cur_line = false,
-	toggle_key = "<leader>ls",
-	handler_opts = {
-		border = "rounded",
-	},
+  bind = true,
+  floating_window_above_cur_line = true,
+  toggle_key = "<leader>ls",
+  handler_opts = {
+    border = "rounded",
+  },
 })
