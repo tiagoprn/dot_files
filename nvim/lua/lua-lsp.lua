cc225c6aca97486131fa9776678b498c6568a26d
tiago.paranhos <tiagoprn@gmail.com>
2022-11-11 09:19:51 -0300

    feat(nvim): configured lua dev environment for nvim, getting rid of "undefined global 'vim' " error

--- nvim/lua/lua-lsp.lua
+++ nvim/lua/lua-lsp.lua
@@ -1,32 +1,38 @@
+-- Here are instructions to setup this plugin: https://github.com/folke/neodev.nvim#-setup
+
+require("neodev").setup({
+	-- add any options here, or leave empty to use the default settings
+})
+
 -- set the path to the sumneko installation
-local sumneko_root_path = "/opt/src/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
+-- local sumneko_root_path = "/opt/src/lua-language-server"
+-- local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
 
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+-- local runtime_path = vim.split(package.path, ";")
+-- table.insert(runtime_path, "lua/?.lua")
+-- table.insert(runtime_path, "lua/?/init.lua")
 
-local luadev = require("neodev").setup({
-	lspconfig = {
-		cmd = { sumneko_binary },
-	},
-	-- key added according to https://jdhao.github.io/2021/08/12/nvim_sumneko_lua_conf/#config-for-nvim-lspconfig
+local lspconfig = require("lspconfig")
+lspconfig.sumneko_lua.setup({
 	settings = {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
 				-- Setup your lua path
-				path = runtime_path,
+				-- path = runtime_path,
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
 				globals = { "vim" },
 			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
+			-- completion = {
+			-- 	callSnippet = "Replace",
+			-- },
+			-- workspace = {
+			--   Make the server aware of Neovim runtime files
+			--   library = vim.api.nvim_get_runtime_file("", true),
+			-- },
 			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
 				enable = false,
@@ -34,6 +40,3 @@ local luadev = require("neodev").setup({
 		},
 	},
 })
-
-local lspconfig = require("lspconfig")
-lspconfig.sumneko_lua.setup(luadev)
