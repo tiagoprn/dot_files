ac69175863466448c8c3f7aea0b7820380a3ccb6
tiago.paranhos <tiagoprn@gmail.com>
2023-01-19 17:23:29 -0300

    feat(nvim): updated lsp_saga setup.

--- nvim/lua/lsp-saga.lua
+++ nvim/lua/lsp-saga.lua
@@ -1,10 +1,12 @@
-local saga = require 'lspsaga'
+local saga = require("lspsaga")
 
-saga.init_lsp_saga {
-  error_sign = 'E',
-  warn_sign = 'W',
-  hint_sign = 'H',
-  infor_sign = 'I',
-  border_style = "round",
-}
-
+saga.setup({
+	code_action_prompt = { -- due to errors when changing buffers. e.g. "Error executing vim.schedule lua callback: .../start/lspsaga.nvim/lua/lspsaga/codeaction/indicator.lua:37: line value outside range"
+		enable = false,
+	},
+	error_sign = "E",
+	warn_sign = "W",
+	hint_sign = "H",
+	infor_sign = "I",
+	border_style = "round",
+})
