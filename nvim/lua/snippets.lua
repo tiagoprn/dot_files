f06e93d215776749e4e0a78609565cbce2302bf7
tiago.paranhos <tiagoprn@gmail.com>
2023-02-03 19:18:30 -0300

    feat(nvim): updated configuration.

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -1,11 +1,10 @@
-require('snippy').setup({
-    mappings = {
-        is = {
-            ['<Tab>'] = 'expand_or_advance',
-            ['<S-Tab>'] = 'previous',
-        },
-        nx = {
-            ['<leader>x'] = 'cut_text',
-        },
-    },
+local snippy = require("snippy")
+
+snippy.setup({
+	mappings = {
+		i = {
+			["<Tab>"] = "expand_or_advance",
+			["<S-Tab>"] = "previous",
+		},
+	},
 })
