6ba80a50976b689b865b9f5f96c53a8131f36206
tiago.paranhos <tiagoprn@gmail.com>
2023-02-18 10:45:11 -0300

    feat(tiling-wms): updated script.

--- tiling-window-managers/scripts/download-remote-wallpaper.sh
+++ tiling-window-managers/scripts/download-remote-wallpaper.sh
@@ -4,8 +4,8 @@ notify-send "Getting remote wallpaper..." \
     && export WALLPAPER_PATH=$HOME/Wallpapers/$(date +%Y%m%d-%H%M%S-%N).jpg \
     && cd /opt/styli.sh \
     &&
-    # ./styli.sh -l reddit && \
-    ./styli.sh -s "$(zenity --entry --text='Choose a theme to download an wallpaper:' --entry-text='space, abstract, nature, earth, ...')" -h 1080 \
+    # styli.sh -l reddit && \
+    styli.sh -s "$(zenity --entry --text='Choose a theme to download an wallpaper:' --entry-text='space, abstract, nature, earth, ...')" -h 1080 \
     && cp ~/.cache/styli.sh/wallpaper.jpg $WALLPAPER_PATH \
     && feh --bg-fill --auto-zoom --scale-down $WALLPAPER_PATH \
     && notify-send "Wallpaper set to $WALLPAPER_PATH ."
