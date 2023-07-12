34272e5f65b1e851e7198723d5802cc3662f8f83
tiago.paranhos <tiagoprn@gmail.com>
2023-07-12 06:13:16 -0300

    feat(tiling-wms): updated .gitconfig for new project and tmux-ide.sh script.

--- tiling-window-managers/scripts/tmux-ide.sh
+++ tiling-window-managers/scripts/tmux-ide.sh
@@ -41,9 +41,6 @@ create_if_needed_and_attach() {
         tmux new-window -t "$session_name:2" -n "git"
         tmux send-keys -t "git" "/storage/src/dot_files/git-ui-with-ssh-agent.sh ." C-m
 
-        tmux new-window -t "$session_name:3" -n "scratchpad"
-        tmux send-keys -t "scratch" "# this is a scratchpad inside the project directory, enjoy!" C-m
-
         echo 'BEFORE ERROR...'
         tmux attach-session -t "$session_name:$DEFAULT_FIRST_WINDOW_INDEX"
     else
