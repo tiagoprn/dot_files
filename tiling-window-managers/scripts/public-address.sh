b82d7be2923639ee2d3b65322ca6ae9e61377143
tiago.paranhos <tiagoprn@gmail.com>
2023-09-19 14:30:55 -0300

    feat(scripts): adding new script to get my public IP address.

--- tiling-window-managers/scripts/public-address.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "$(curl -s ifconfig.me/ip)"
-
---
+++ tiling-window-managers/scripts/public-ip-address.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Fetch data from the two services
+output1=$(curl -s https://ifconfig.me/all.json)
+output2=$(curl -s https://ipinfo.io)
+
+if [[ $1 == "--only-ip" ]]; then
+    # Try to extract an IPv4 address from ifconfig_me
+    ipv4_from_ifconfig=$(echo "$output1" | jq -r '.ip_addr | select(. | contains(":") | not)')
+
+    # If IPv4 is found in ifconfig_me, store in final_output, else use the IP from ipinfo_io
+    if [[ -n $ipv4_from_ifconfig ]]; then
+        final_output="$ipv4_from_ifconfig"
+    else
+        final_output=$(echo "$output2" | jq -r '.ip')
+    fi
+else
+    # If no arguments, merge the JSONs
+    final_output=$(jq -n \
+        --argjson ifconfig_me "$output1" \
+        --argjson ipinfo_io "$output2" \
+        '{ifconfig_me: $ifconfig_me, ipinfo_io: $ipinfo_io}')
+fi
+
+echo "$final_output" | xclip -selection clipboard
+echo "$final_output" | xclip -selection primary
+
+notify-send "Successfully copied IP data to clipboard."
