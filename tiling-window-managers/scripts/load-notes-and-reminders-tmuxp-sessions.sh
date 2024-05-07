2dba4f20aff9b4655a278510a9dd288f147ebc88
tiago.paranhos <tiagoprn@gmail.com>
2024-05-07 07:56:07 -0300

    feat(tiling wms): updated script with new tmux session.

--- tiling-window-managers/scripts/load-notes-and-reminders-tmuxp-sessions.sh
+++ tiling-window-managers/scripts/load-notes-and-reminders-tmuxp-sessions.sh
@@ -2,4 +2,5 @@
 
 tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/fleeting-notes.yml &
 tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/reminders.yml &
+tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/ai-prompts.yml &
 tmuxp load /storage/src/devops/tmuxp/notes-and-reminders/tasks.yml
