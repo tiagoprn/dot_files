245d3dcff54ec4480dc56855cbcb68d7b2692147
tiago.paranhos <tiagoprn@gmail.com>
2024-06-14 14:29:56 -0300

    fix(tiling-wms): excluded the current session name from the list of sessions.

--- tiling-window-managers/scripts/switch-tmux-session.sh
+++ tiling-window-managers/scripts/switch-tmux-session.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Below orders the tmux sessions by last attached
+
+CURRENT_SESSION=$(tmux display-message -p '#S')
+
 tmux list-sessions -F '#{session_last_attached} #{session_name}' \
+    | grep -vw "$CURRENT_SESSION" \
     | sort -rn \
     | cut -d' ' -f2- \
     | fzf -e --header="Select a tmux session to attach:" \
