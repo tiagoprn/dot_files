a9d74142ffd050a61ca3e730cdddfddc12a54727
tiago.paranhos <tiagoprn@gmail.com>
2024-06-30 18:15:15 -0300

    feat(wayland): updated powermenu script

--- wayland/scripts/powermenu-hypr.sh
+++ wayland/scripts/powermenu-hypr.sh
@@ -3,26 +3,29 @@
 
 option1="  lock"
 option2="  logout"
-option3="  restart"
-option4="  power off"
+option3="  restart gdm "
+option4="  restart machine"
+option5="  power off"
 
-options="$option1\n"
-options="$options$option2\n"
-options="$options$option3\n$option4"
+options="$option1\n$option2\n$option3\n$option4\n$option5\n"
 
 choice=$(echo -e "$options" | wofi -dmenu -i -l 4 -width 10 -p "Choose wisely")
 
 case $choice in
     $option1)
-        swaylock
+        # swaylock
+        notify-send "NOT IMPLEMENTED YET, CHOOSE ANOTHER!"
         ;;
     $option2)
         hyprctl dispatch exit
         ;;
     $option3)
-        sudo shutdown -r now
+        sudo systemctl restart gdm
         ;;
     $option4)
+        sudo shutdown -r now
+        ;;
+    $option5)
         sudo shutdown -h now
         ;;
 esac
