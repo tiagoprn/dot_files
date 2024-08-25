d96e4be0e36e21fc8c7c6730cc8e17197a70f955
tiago.paranhos <tiagoprn@gmail.com>
2024-08-25 07:01:56 -0300

    feat: updated bash welcome scripts with colors.

--- bash-welcome.sh
+++ bash-welcome.sh
@@ -2,6 +2,10 @@
 
 set -eou pipefail
 
+# Define color variables
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # if [ -d "/storage/src/fortunes/" ]; then
 #     cowsay -b -f tux -W 80 $(fortune -u -e -c /storage/src/fortunes/ | tail +3)
 # fi
@@ -20,4 +24,6 @@ echo -e '  with nvim, gitui and a scratchpad.'
 #     echo -e '- To run timeshift: sudo timeshift-gtk'
 # fi
 
-echo -e '==> To call navi, use <CTRL+G> when on INSERT mode. <=='
+echo -e "-----"
+echo -e "==> To get password from ${RED}pass${NC}, use the ${RED}gp${NC} bash alias <=="
+echo -e "==> To call ${RED}navi${NC}, use ${RED}<CTRL+G>${NC} when on INSERT mode. <=="
