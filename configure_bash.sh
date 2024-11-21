1516e55b4cc62418d7c0726e661d0f68c84e55d3
tiago.paranhos <tiagoprn@gmail.com>
2024-11-21 07:12:25 -0300

    feat(scripts): added television to the configure_bash bootstrap script

--- configure_bash.sh
+++ configure_bash.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 TIMESTAMP="$(date "+%Y%m%d.%H%M.%S")"
 mv ~/.bash_aliases ~/.bash_aliases.$TIMESTAMP.snapshot || true
 mv ~/.bash_functions ~/.bashrc_functions.$TIMESTAMP.snapshot || true
@@ -10,7 +11,7 @@ mv ~/.fzf ~/.fzf.$TIMESTAMP.snapshot || true
 mv ~/.xsession ~/.xsession.$TIMESTAMP.snapshot || true
 
 mv ~/.config/pudb/pudb.cfg ~/.config/pudb/pudb.cfg.$TIMESTAMP.snapshot || true
-
+mv ~/.config/television ~/.config/television.$TIMESTAMP.snapshot || true
 
 ln -s /storage/src/dot_files/.bash_aliases ~/.bash_aliases
 ln -s /storage/src/dot_files/.bash_functions ~/.bash_functions
@@ -21,7 +22,8 @@ ln -s /storage/src/dot_files/.fonts ~/.fonts
 ln -s /storage/src/dot_files/.fzf ~/.fzf
 ln -s /storage/src/dot_files/.xsession ~/.xsession
 ln -s /storage/src/dot_files/.navirc ~/.navirc
-mkdir -p ~/.config/pudb || true && ln -s /storage/src/dot_files/pudb/pudb.cfg ~/.config/pudb/pudb.cfg  || true
+
+mkdir -p ~/.config/pudb || true && ln -s /storage/src/dot_files/pudb/pudb.cfg ~/.config/pudb/pudb.cfg || true
+mkdir -p ~/.config/television || true && ln -s /storage/src/dot_files/television ~/.config/television || true
 
 fc-cache -vf ~/.fonts/ && echo 'listing fonts...' && fc-list
-
