e63f70844ccb919a36f1a306694a5a923ea5d76e
tiago.paranhos <tiagoprn@gmail.com>
2025-04-01 15:07:53 -0300

    chore(scripts): changed order of tmuxp-notes_tasks_n_reminders.sh to load the codex tmuxp last.

--- wayland/scripts/tmuxp-notes_tasks_n_reminders.sh
+++ wayland/scripts/tmuxp-notes_tasks_n_reminders.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-TMUXP_SESSIONS_FILES="/storage/src/devops/tmuxp/notes-and-reminders/fleeting-notes.yml /storage/src/devops/tmuxp/notes-and-reminders/ai-prompts.yml /storage/src/devops/tmuxp/notes-and-reminders/writeloop-raw.yml /storage/src/devops/tmuxp/notes-and-reminders/codex.yml /storage/src/devops/tmuxp/notes-and-reminders/reminders.yml /storage/src/devops/tmuxp/notes-and-reminders/tasks.yml"
+TMUXP_SESSIONS_FILES="/storage/src/devops/tmuxp/notes-and-reminders/fleeting-notes.yml /storage/src/devops/tmuxp/notes-and-reminders/ai-prompts.yml /storage/src/devops/tmuxp/notes-and-reminders/writeloop-raw.yml /storage/src/devops/tmuxp/notes-and-reminders/reminders.yml /storage/src/devops/tmuxp/notes-and-reminders/tasks.yml /storage/src/devops/tmuxp/notes-and-reminders/codex.yml"
 tmuxp load $TMUXP_SESSIONS_FILES
