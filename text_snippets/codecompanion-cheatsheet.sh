ff597972994394f3170b1fee6f012b6b8915bf70
tiago.paranhos <tiagoprn@gmail.com>
2025-04-10 07:00:39 -0300

    feat(text_snippets): updated

--- text_snippets/codecompanion-cheatsheet.sh
+++ text_snippets/codecompanion-cheatsheet.sh
@@ -4,11 +4,9 @@
 CONTENT_FILE="/storage/src/devops/cheats/html/codecompanion.html"
 
 # Read the file, strip excessive whitespace and newlines, and format for zenity
-FORMATTED_CONTENT=$(cat "$CONTENT_FILE" | tr '\n' ' ')
-
-echo "$FORMATTED_CONTENT"
+FORMATTED_CONTENT=$(tr '\n' ' ' <"$CONTENT_FILE")
 
 # Display the cheatsheet using zenity
-zenity --info --title="CodeCompanion Cheatsheet" --text="$FORMATTED_CONTENT" --ok-label="Got it!" --width=900
+zenity --info --title="CodeCompanion Cheatsheet" --text="$FORMATTED_CONTENT" --ok-label="Got it!" --width=600
 
-print '' # this is only to not copy anything to the clipboard, since I only want to show the cheatsheet.
+echo '' # this is only to not copy anything to the clipboard, since I only want to show the cheatsheet.
