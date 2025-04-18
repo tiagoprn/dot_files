21558ca59afa0e0e784cb0d40f7a220a66fe0abf
tiago.paranhos <tiagoprn@gmail.com>
2025-04-18 16:32:45 -0300

    feat(scripts): adding custom message

--- wayland/scripts/bw-login.sh
+++ wayland/scripts/bw-login.sh
@@ -9,6 +9,7 @@ SELECTED_CONFIG=$(find $BW_CONFIG_ROOT/ -type f -not -name ".*" | fzf --exact)
 BW_EMAIL=$(cat $SELECTED_CONFIG)
 PASS_ENTRY="bitwarden/$(basename $SELECTED_CONFIG)" # ← path in `pass` where your BW password is stored
 SESSION_FILE="$BW_CONFIG_ROOT/.bw_session"          # ← where to save session
+CUSTOM_LOGIN_MESSAGE_FILE="$BW_CONFIG_ROOT/.custom_login_message"
 
 echo "pass key: $PASS_ENTRY"
 echo "bitwarden email: $BW_EMAIL"
@@ -17,6 +18,10 @@ echo "bitwarden email: $BW_EMAIL"
 if ! command -v pass >/dev/null; then
     echo "❌ 'pass' command not found." >&2
     exit 1
+fi
+
+if [ -f "$CUSTOM_LOGIN_MESSAGE_FILE" ]; then
+    cat "$CUSTOM_LOGIN_MESSAGE_FILE"
 fi
 
 # === LOGIN ===
