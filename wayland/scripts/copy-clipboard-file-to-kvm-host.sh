7af109ebcabca37c707ebdcd455b333250bd5ae6
tiago.paranhos <tiagoprn@gmail.com>
2025-12-17 11:37:55 -0300

    feat(wayland): updated script

--- wayland/scripts/copy-clipboard-file-to-kvm-host.sh
+++ wayland/scripts/copy-clipboard-file-to-kvm-host.sh
@@ -2,6 +2,8 @@
 
 set -eou pipefail
 
+WATCHEXEC_BIN=$HOME/.cargo/bin/watchexec
+
 mkdir -p /tmp/clipboard || true \
     && echo "Monitor clipboard file script successfully started on $(hostname)!" >/tmp/clipboard/copied.txt \
-    && watchexec --watch /tmp/clipboard --filter 'copied.txt' time rsync --info=progress2 /tmp/clipboard/copied.txt enterprise-d:/tmp/clipboard/copied.txt
+    && $WATCHEXEC_BIN --watch /tmp/clipboard --filter 'copied.txt' time rsync --info=progress2 /tmp/clipboard/copied.txt enterprise-d:/tmp/clipboard/copied.txt
