cdeab09e4432e1e56955b758ce5fcd7c23634f06
tiago.paranhos <tiagoprn@gmail.com>
2025-12-17 09:43:01 -0300

    feat: creating log file path

--- wayland/scripts/monitor-clipboard-file.sh
+++ wayland/scripts/monitor-clipboard-file.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p /tmp/clipboard || true \
+mkdir -p "$HOME/.local/state/hyprland" || true \
+    && mkdir -p /tmp/clipboard || true \
     && echo 'Monitor clipboard file script successfully started!' >/tmp/clipboard/copied.txt \
     && watchexec --watch /tmp/clipboard --filter 'copied.txt' /storage/src/dot_files/wayland/scripts/copy-file-clipboard-to-wayland-clipboard.sh &
