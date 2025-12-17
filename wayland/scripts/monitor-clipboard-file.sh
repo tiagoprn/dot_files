f4cc8e6b2f1ea22a43e82d3585ca43dc965fe38e
tiago.paranhos <tiagoprn@gmail.com>
2025-12-17 09:19:11 -0300

    fix: bug when running on hyprland autobool

--- wayland/scripts/monitor-clipboard-file.sh
+++ wayland/scripts/monitor-clipboard-file.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-watchexec --watch /tmp/clipboard --filter 'copied.txt' /storage/src/dot_files/wayland/scripts/copy-file-clipboard-to-wayland-clipboard.sh &
+mkdir -p /tmp/clipboard || true \
+    && echo 'Monitor clipboard file script successfully started!' >/tmp/clipboard/copied.txt \
+    && watchexec --watch /tmp/clipboard --filter 'copied.txt' /storage/src/dot_files/wayland/scripts/copy-file-clipboard-to-wayland-clipboard.sh &
