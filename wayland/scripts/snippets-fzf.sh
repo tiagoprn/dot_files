7e0ecd17d6fff709197a9b3103c74c21e80828a6
tiago.paranhos <tiagoprn@gmail.com>
2025-12-17 16:30:32 -0300

    refac(hypr,scripts): changed binds, snippets script removed trailing new lines

--- wayland/scripts/snippets-fzf.sh
+++ wayland/scripts/snippets-fzf.sh
@@ -25,6 +25,12 @@ if [ -z "${DATA:-}" ]; then
     exit 1
 fi
 
+# Strip one trailing newline if present
+DATA="${DATA%$'\n'}"
+
+# Optionally also strip a trailing carriage return (for CRLF files)
+DATA="${DATA%$'\r'}"
+
 printf '%s' "$DATA" >/tmp/clipboard/copied.txt
 
 notify-send -t 5000 "🤖 Snippet successfully processed and copied to clipboard! Now, paste it where you need. 📋"
