896957147e6c1fa59567b96a5d0bce3c44237afd
tiago.paranhos <tiagoprn@gmail.com>
2026-01-12 11:43:14 -0300

    feat: updated files related to the new zk journal feature.

--- wayland/scripts/journal.sh
+++ wayland/scripts/journal.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 
-# +"normal Go": Moves to the end of the file and inserts a blank line.
-# +"normal o---": Adds a line with ---.
-# +"normal o# $(date +"%Y %B %d %A %H:%M:%S")": Adds the date as # YYYY Month DD Day HH:MM:SS.
-# +"normal o": Adds another blank line.
-# +"normal o": Adds a second blank line before switching to insert mode.
-# +"startinsert": Enters insert mode so you can start typing.
-
-nvim +"normal Go" \
-    +"normal o---" \
-    +"normal o# $(date +"%Y %B %d %A %H:%M:%S")" \
-    +"normal o" \
-    +"normal o" \
-    +"startinsert" \
-    "$HOME/journal.$(hostname).md"
+cd /storage/src/mindshards \
+    && clear \
+    && echo -e "Will open nvim to create the shard file.\nRun - zk w - to push to the repo;\nRun - zk f - to pull from the repo." \
+    && read -n1 -s -r -p "Press any key to continue..." \
+    && zk shard
