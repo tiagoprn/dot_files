585282e74fcc9a11013258f3a85aa171103d1774
tiago.paranhos <tiagoprn@gmail.com>
2026-02-20 11:11:52 -0300

    feat: removing h

--- wayland/scripts/ai-fabric-clipboard-file-actions.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ACTION=$(echo -e 'summarize\ncustom_spell_check\nrewrite_text' | fzf --prompt 'Choose a fabric ai pattern: ')
-
-cat /tmp/clipboard/copied.txt | fabric-ai -p "$ACTION" >/tmp/clipboard/ai.txt && nvim -o /tmp/clipboard/copied.txt /tmp/clipboard/ai.txt
