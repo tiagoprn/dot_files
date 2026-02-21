c545ace6b3978b82edd7132d4821d267b0e38605
tiago.paranhos <tiagoprn@gmail.com>
2026-02-21 07:49:56 -0300

    chore(bash-welcome): final tweaks.

--- bash-welcome.sh
+++ bash-welcome.sh
@@ -6,6 +6,8 @@ set -eou pipefail
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
+BOLD='\033[1m'
+BOLD_GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 clear && reset
@@ -20,12 +22,12 @@ else
     echo -e '==> TMUX GOODIES:'
     echo -e "    - Use ${YELLOW}tmux-ide.sh${NC} to create an ide session ${YELLOW}in the current directory${NC}."
     echo -e "    - Use ${YELLOW}dots.sh${NC} to create tmux sessions to all my config repos (${YELLOW}dot_files, pde.nvim, etc${NC})."
-    echo -e "    - To open tmux ${YELLOW}mindshards & codex & reminders${NC}, use the ${YELLOW}hi${NC} bash alias <=="
+    echo -e "    - To open tmux ${YELLOW}mindshards & codex & reminders${NC}, use the ${YELLOW}hi${NC} bash alias "
 fi
 
-echo -e "==> To get password from ${YELLOW}pass${NC}, use the ${YELLOW}gp${NC} bash alias <=="
-echo -e "==> To ${YELLOW}edit local file clipboard${NC}, use the ${YELLOW}cf${NC} bash alias<=="
-echo -e "==> To ${YELLOW}restart tars remotely${NC}, use the ${YELLOW}rt${NC} bash alias<=="
-echo -e "==> Before running the ${GREEN}pi coding agent${NC} on a ${GREEN}non-git directory${NC}, use the ${GREEN}cds${NC} bash alias<=="
+echo -e "==> To get password from ${YELLOW}pass${NC}, use the ${YELLOW}gp${NC} bash alias "
+echo -e "==> To ${YELLOW}edit local file clipboard${NC}, use the ${YELLOW}cf${NC} bash alias"
+echo -e "==> To ${YELLOW}restart tars remotely${NC}, use the ${YELLOW}rt${NC} bash alias"
+echo -e "==> Before running the ${BOLD_GREEN}pi coding agent${NC} on a ${BOLD_GREEN}non-git directory${NC}, use the ${BOLD_GREEN}cds${NC} bash alias"
 echo -e "==============================================================================================================================="
 echo -e ""
