8590252feca44356a1ac2d79e1a2ed3734c33e5c
tiago.paranhos <tiagoprn@gmail.com>
2026-02-21 07:44:27 -0300

    feat(bash-welcome): updated

--- bash-welcome.sh
+++ bash-welcome.sh
@@ -8,22 +8,24 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-echo -e 'TMUX GOODIES:'
+clear && reset
 
-# is not inside tmux:
+echo -e "==============================================================================================================================="
+echo -e "                                               WELCOME TO $(hostname)!"
+echo -e "==============================================================================================================================="
+# if we are not inside tmux:
 if [ -z "${TMUX+set}" ]; then
-    echo -e "- Open ${YELLOW}tmuxp available sessions${NC} with the ${YELLOW}tp${NC} bash alias. (e.g. REMOTE)"
+    echo -e "==> Open ${GREEN}tmuxp available sessions${NC} with the ${GREEN}tp${NC} bash alias. (e.g. REMOTE)"
+else
+    echo -e '==> TMUX GOODIES:'
+    echo -e "    - Use ${YELLOW}tmux-ide.sh${NC} to create an ide session ${YELLOW}in the current directory${NC}."
+    echo -e "    - Use ${YELLOW}dots.sh${NC} to create tmux sessions to all my config repos (${YELLOW}dot_files, pde.nvim, etc${NC})."
+    echo -e "    - To open tmux ${YELLOW}mindshards & codex & reminders${NC}, use the ${YELLOW}hi${NC} bash alias <=="
 fi
 
-echo -e "- Use ${YELLOW}tmux-ide.sh${NC} to create an ide session ${YELLOW}in the current directory${NC}."
-echo -e "- Use ${YELLOW}dots.sh${NC} to create tmux sessions to all my config repos "
-echo -e "  (${YELLOW}dot_files, devops, pde.nvim & nix-home-manager${NC})."
-
-echo -e "-------------------------------------------------------------------------------------------------------------------------------"
 echo -e "==> To get password from ${YELLOW}pass${NC}, use the ${YELLOW}gp${NC} bash alias <=="
-echo -e "==> To open tmux ${YELLOW}mindshards & codex & reminders${NC}, use the ${YELLOW}hi${NC} bash alias <=="
 echo -e "==> To ${YELLOW}edit local file clipboard${NC}, use the ${YELLOW}cf${NC} bash alias<=="
 echo -e "==> To ${YELLOW}restart tars remotely${NC}, use the ${YELLOW}rt${NC} bash alias<=="
 echo -e "==> Before running the ${GREEN}pi coding agent${NC} on a ${GREEN}non-git directory${NC}, use the ${GREEN}cds${NC} bash alias<=="
-echo -e "-------------------------------------------------------------------------------------------------------------------------------"
+echo -e "==============================================================================================================================="
 echo -e ""
