acc81429a53ae146a7fd1728869618bef497dc11
tiago.paranhos <tiagoprn@gmail.com>
2026-02-23 15:36:38 -0300

    feat(dots.sh): updated

--- wayland/scripts/dots.sh
+++ wayland/scripts/dots.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-WORDS="dot_files devops nix-home-manager pde.nvim"
+WORDS="dot_files devops nix-home-manager pde.nvim aikt"
 
 for WORD in $WORDS; do
     # the "n" argument will make it only create the sessions, not attach to them.
